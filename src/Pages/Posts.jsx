c4c18be59ed8c4e4b8bc65dd04a68773e21828f4
D-Bars <awotik26@gmail.com>
2025-02-11 23:15:08 +0100

    add: sort posts(need styles)

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,35 +1,59 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import useFetchPosts from '../components/hooks/useFetchPosts';
 import cl from './style/Posts.module.scss';
 import ErrorMessage from '../components/ErrorMessage';
 import Loader from '../components/UI/Loader/Loader';
-import SearchBox from '../components/SearchBox';
 import PostsItemsList from '../components/PostsItemsList';
 import Pagination from '../components/UI/Pagination/Pagination';
 import { paginatePosts } from '../utils/paginate';
+import MyInput from '../components/UI/Input/MyInput';
+import MySelect from '../components/UI/Select/MySelect';
 
 const Posts = () => {
     const { posts, error, postsLoading } = useFetchPosts();
 
     const [limitPosts, setLimitPosts] = useState(10);
     const [page, setPage] = useState(1);
+    const [selectedSort, setSelectedSort] = useState('');
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+    }
+    
+    const sortedPosts = useMemo(() => {
+        if (!selectedSort) return posts;
+    
+        return [...posts].sort((a, b) => {
+            const aValue = a.breeds?.[0]?.[selectedSort] ?? '';  
+            const bValue = b.breeds?.[0]?.[selectedSort] ?? '';  
+            return aValue.localeCompare(bValue);
+        });
+    }, [posts, selectedSort]);
 
     const postsBlockRef = useRef(null);
-
 
     const changePage = (newPage) => {
         setPage(newPage);
         postsBlockRef.current.scrollIntoView({ behavior: 'smooth' });
     }
 
-    const paginationPosts = paginatePosts(posts, limitPosts, page);
+    const paginationPosts = paginatePosts(sortedPosts, limitPosts, page);
 
     return (
         <div
             ref={postsBlockRef}
             className={cl.posts_block}
         >
-            <SearchBox></SearchBox>
+            <MyInput placeholder='search...' />
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultVal='Sort by'
+                options={[
+                    { value: 'name', name: 'Sort by name' },
+                    { value: 'breed_group', name: 'Sort by bred' }
+                ]}
+            ></MySelect>
             {postsLoading ? (
                 <Loader></Loader>
             ) :
---
+++ src/components/UI/Select/MySelect.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cl from './MySelect.module.scss'
+
+const MySelect = ({ options, defaultVal, value, onChange }) => {
+    // console.log(value)
+    return (
+        <select
+            value={value}
+            onChange={event=> onChange(event.target.value)}
+            id={cl.select}
+        >
+            <option disabled value="">{defaultVal}</option>
+            {options.map(option => (
+                <option key={option.value} value={option.value}>
+                    {option.name}
+                </option>
+            ))}
+        </select>
+    );
+};
+export default MySelect;
